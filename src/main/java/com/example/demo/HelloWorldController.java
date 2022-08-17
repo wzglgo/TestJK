dc0976e27155731747c4dae0012ecbfb8af10b32
wzglgo <95947852+wzglgo@users.noreply.github.com>
2022-08-17 16:16:30 +0800

    Add files via upload

---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/helloWorld")
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return  "helloWorld!!!";
+    }
+}
